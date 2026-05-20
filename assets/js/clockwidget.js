9947fd9cd44f7e5951a9312854d823de2b2fd7ea
sirbobisherenow <chetan.likes.nature@gmail.com>
2026-05-20 08:43:50 -0700

    add font, clock widget

---
+++ assets/js/clockwidget.js
@@ -0,0 +1,24 @@
+function updateClock() {
+    //get the element time, and store it as variable
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+    
+    //get the time
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    
+    // padding
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    
+    //stringify
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    
+    // set the element thing
+    timeElement.textContent  = timeString;
+}
+
+// update every seconde
+setInterval(updateClock, 1000);
